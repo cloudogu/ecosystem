efcbcac1ddfb08facb77efa89e8db74c1cb8efb3
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-01-27 15:58:53 +0100

    added container for scm-manager

---
+++ dockerfile/scm/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATADIR="/var/lib/ces/scm"
+if [ ! -d "$DATADIR" ]; then
+  btrfs subvolume create "$DATADIR"
+  mkdir "$DATADIR/data"
+  chown -R 1000:1000 "$DATADIR"
+  chmod -R 755 "$DATADIR"
+fi
+
+docker rm scm
+docker run -d \
+  --name scm \
+  -h scm \
+  -P \
+  -v /etc/ces:/etc/ces \
+  -v "$DATADIR/data":/var/lib/scm \
+  cesi/scm
