1ecfa65e08ccfc539917559d227f1e48020e8a8b
Stephan Christann <schristann@linux-51pf.site>
2015-01-28 10:49:20 +0100

    Added btrfs subvolume support for mysql docker container

--- dockerfile/mysql/run.sh
+++ dockerfile/mysql/run.sh
@@ -3,8 +3,11 @@
 dbDir="/var/lib/ces/mysql"
 
 if ! [ -d $dbDir ]; then
-	mkdir -p $dbDir
-	chown 1000:1000 $dbDir
+	btrfs subvolume create $dbDir
+	if ! [ -d $dbDir/data ]; then
+		mkdir $dbDir/data
+		chown 1000:1000 $dbDir/data
+	fi
 fi
 
-docker run -d -p 3306:3306 -v $dbDir:/var/lib/mysql cesi/mysql
+docker run -d -p 3306:3306 -v $dbDir/data:/var/lib/mysql cesi/mysql
