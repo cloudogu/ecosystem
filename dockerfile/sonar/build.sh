db702dbb1f09d33fcfd42e74253aa77ee97e821b
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-01-28 11:44:01 +0100

    started sonar container

---
+++ dockerfile/sonar/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t cesi/sonar $(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
---
+++ dockerfile/sonar/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATADIR="/var/lib/ces/sonar"
+if [ ! -d "$DATADIR" ]; then
+  btrfs subvolume create "$DATADIR"
+  mkdir "$DATADIR/data"
+  chown -R 1000:1000 "$DATADIR"
+  chmod -R 755 "$DATADIR"
+fi
+
+docker rm sonar
+docker run -d \
+  --name sonar \
+  -h sonar \
+  -P \
+  -v /etc/ces:/etc/ces \
+  -v "$DATADIR/data":/var/lib/sonar \
+  cesi/sonar
