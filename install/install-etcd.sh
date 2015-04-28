4f80e57e96df0c4aeb9ac2c0fd99a149007dfce8
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-28 14:42:13 +0200

    update etcd to version 2.0.10

--- install/install-etcd.sh
+++ install/install-etcd.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ETCD_VERSION=v2.0.10
+
 function install_etcd(){
-  curl -L  https://github.com/coreos/etcd/releases/download/v2.0.0/etcd-v2.0.0-linux-amd64.tar.gz -o /tmp/etcd.tar.gz
+  curl -L  https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -o /tmp/etcd.tar.gz
   mkdir -p /opt/ces/etcd
   tar xfz /tmp/etcd.tar.gz -C /opt/ces/etcd --strip-components=1
   mkdir -p /opt/ces/bin
   ln -s /opt/ces/etcd/etcdctl /opt/ces/bin/etcdctl
-  rm -f /tmp/etcd-v2.0.0-linux-amd64.tar.gz
+  rm -f /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
 }
 
 if [ ! -d "/opt/ces/etcd" ]; then
