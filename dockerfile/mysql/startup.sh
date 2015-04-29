46c05e6bf08d257db3bb2c237702619d296bc2c1
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-29 11:53:35 +0200

    improve mysql installation

--- dockerfile/mysql/startup.sh
+++ dockerfile/mysql/startup.sh
@@ -7,7 +7,11 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
     sleep 10s
     source /etc/ces/functions.sh
     MYSQL_ROOT_PASSWORD=$(create_or_get_ces_pass mysql_root)
-    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY \"${MYSQL_ROOT_PASSWORD}\" WITH GRANT OPTION"
+    # http://www.jbmurphy.com/2011/06/20/mysql_secure_installation/
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY \"${MYSQL_ROOT_PASSWORD}\" WITH GRANT OPTION;"
+    mysql -uroot -e "DROP DATABASE test;"
+    mysql -uroot -e "DELETE FROM mysql.user WHERE User=\";\""
+    mysql -uroot -e "FLUSH PRIVILEGES;"
     sleep 2
     killall mysqld
     sleep 10s
