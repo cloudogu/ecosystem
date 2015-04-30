bc8d2e0078958be0ef08096269e66ec0214c35ce
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-30 14:10:45 +0200

    set universeadm stage and truststore

--- containers/universeadm/docker/resources/setenv.sh
+++ containers/universeadm/docker/resources/setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-UNIVERSEADM_HOME="/var/lib/universeadm/conf"
+JAVA_OPTS="$JAVA_OPTS -Duniverseadm.stage=PRODUCTION"
+JAVA_OPTS="$JAVA_OPTS -Duniverseadm.home=/var/lib/universeadm/conf"
 JAVA_OPTS="$JAVA_OPTS -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks"
 JAVA_OPTS="$JAVA_OPTS -Djavax.net.ssl.trustStorePassword=changeit"
