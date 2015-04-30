4b03bdcc25468c7287ba3c4a13766d4069208121
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-30 14:27:40 +0200

    prefer ipv4 for universeadm tomcat

--- containers/universeadm/docker/resources/setenv.sh
+++ containers/universeadm/docker/resources/setenv.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
+JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"
 JAVA_OPTS="$JAVA_OPTS -Duniverseadm.stage=PRODUCTION"
 JAVA_OPTS="$JAVA_OPTS -Duniverseadm.home=/var/lib/universeadm/conf"
 JAVA_OPTS="$JAVA_OPTS -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks"
