260d86349b7a07befa9152eba4119688f2d4425c
Sebastian Sdorra <sebastian.sdorra@triology.de>
2015-04-30 14:14:06 +0200

    start jvm headless

--- containers/universeadm/docker/resources/setenv.sh
+++ containers/universeadm/docker/resources/setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
 JAVA_OPTS="$JAVA_OPTS -Duniverseadm.stage=PRODUCTION"
 JAVA_OPTS="$JAVA_OPTS -Duniverseadm.home=/var/lib/universeadm/conf"
 JAVA_OPTS="$JAVA_OPTS -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks"
