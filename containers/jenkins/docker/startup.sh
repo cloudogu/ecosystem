9838e3b409628021742da15255974feb595b68c5
Robert Auer <robert.auer@triology.de>
2015-10-27 14:54:09 +0100

    Cleaned up startup.sh

--- containers/jenkins/docker/startup.sh
+++ containers/jenkins/docker/startup.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-echo "Checking if /var/lib/jenkins/cas-plugin exists..."
+# Checking if /var/lib/jenkins/cas-plugin exists
 if [ ! -f /var/lib/jenkins/plugins/cas-plugin.hpi ]; then
-	echo "Making directory, if not already existing"
+	# Making directory, if not already existing
 	mkdir -p /var/lib/jenkins/plugins
-        echo "Copying /cas-plugin.hpi to /var/lib/jenkins/plugins"
+	# Copy plugin
         cp /cas-plugin.hpi /var/lib/jenkins/plugins/
 fi
-#echo "startup script is done."
-echo "Starting jenkins now..."
 
-
-
+# starting jenkins
 java -Djava.awt.headless=true \
   -Djava.net.preferIPv4Stack=true \
   -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks \
   -Djavax.net.ssl.trustStorePassword=changeit \
   -jar /jenkins.war --prefix=/jenkins
-
-#sleep 100
-
-#if [ ! -d /var/lib/jenkins/plugins ]; then
-#        echo "Directory /var/lib/jenkins/plugins does not exist."
-#	mkdir -p /var/lib/jenkins/plugins
-#fi
-
-#echo "Checking if /var/lib/jenkins/cas-plugin exists..."
-#if [ ! -f /var/lib/jenkins/plugins/cas-plugin.hpi ]; then
-#        echo "Copying /cas-plugin.hpi to /var/lib/jenkins/plugins"
-#        cp /cas-plugin.hpi /var/lib/jenkins/plugins/
-#fi
-#echo "startup script is done."
