b6b58b6a7fc001de02e09bc6fa845536d44fba4b
Robert Auer <robert.auer@triology.de>
2016-01-26 14:11:52 +0100

    Added fundamental files for redmine container; #20

---
+++ containers/redmine/docker/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /etc/ces/functions.sh
+
+# create environment for templates
+FQDN=$(get_fqdn)
+
+# starting redmine
+
+
+
+
+
+#java -Djava.awt.headless=true \
+#  -Djava.net.preferIPv4Stack=true \
+#  -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks \
+#  -Djavax.net.ssl.trustStorePassword=changeit \
+#  -jar /jenkins.war --prefix=/jenkins
