3b5c92ed2bb68b57b0db95f4f5c1dd89eaf0435a
Robert Auer <robert.auer@triology.de>
2016-01-27 13:59:39 +0100

    Redmine database is created in mysql; #20

--- containers/redmine/docker/startup.sh
+++ containers/redmine/docker/startup.sh
@@ -14,7 +14,7 @@ MYSQL_USER="redmine"
 MYSQL_USER_PASSWORD=$(create_or_get_ces_pass mysql_redmine)
 MYSQL_DB="redmine"
 # prepare database
-if [ $(mysql -N -s -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" -e "select count(*) from information_schema.tables where table_schema='${MYSQL_DB}';") -eq 1 ]; then
+if [ $(mysql -N -s -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" -e "select count(*) from information_schema.schemata where schema_name='${MYSQL_DB}';") -eq 1 ]; then
   echo "redmine database is already installed"
 else
   echo "install redmine database"
