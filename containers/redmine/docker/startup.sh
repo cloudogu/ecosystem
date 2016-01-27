c7d97f48312578fbd0979d62066fb8843c8b9b9b
Robert Auer <robert.auer@triology.de>
2016-01-27 13:04:56 +0100

    Added redmine startup.sh script; #20

--- containers/redmine/docker/startup.sh
+++ containers/redmine/docker/startup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 source /etc/ces/functions.sh
 
-# create environment for templates
+# install redmine database
+#MYSQL_ROOT_PASSWORD=$(create_or_get_ces_pass mysql_root)
+
+
+# get variables for templates
 FQDN=$(get_fqdn)
+MYSQL_IP=mysql
+MYSQL_ADMIN="root"
+MYSQL_ADMIN_PASSWORD=$(get_ces_pass mysql_root)
+MYSQL_USER="redmine"
+MYSQL_USER_PASSWORD=$(create_or_get_ces_pass mysql_redmine)
+MYSQL_DB="redmine"
+# prepare database
+if [ $(mysql -N -s -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" -e "select count(*) from information_schema.tables where table_schema='${MYSQL_DB}';") -eq 1 ]; then
+  echo "redmine database is already installed"
+else
+  echo "install redmine database"
+  mysql -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" -e "CREATE DATABASE ${MYSQL_DB} CHARACTER SET utf8;"
+  mysql -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" -e "CREATE USER '${MYSQL_USER}'@'${MYSQL_IP}' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+  mysql -h "${MYSQL_IP}" -u "${MYSQL_ADMIN}" "-p${MYSQL_ADMIN_PASSWORD}" "${MYSQL_DB}" -e "grant all on ${MYSQL_DB}.* to \"${MYSQL_USER}\"@\"172.17.%\" identified by \"${MYSQL_USER_PASSWORD}\";FLUSH PRIVILEGES;"
+fi
 
-# starting redmine
+# adjust redmine database.yml
+sed -i "s/\"\"/${MYSQL_USER_PASSWORD}/g" /usr/src/redmine/config/database.yml
 
 
-
-
-
-#java -Djava.awt.headless=true \
-#  -Djava.net.preferIPv4Stack=true \
-#  -Djavax.net.ssl.trustStore=/etc/ces/ssl/truststore.jks \
-#  -Djavax.net.ssl.trustStorePassword=changeit \
-#  -jar /jenkins.war --prefix=/jenkins
+# start redmine
+rails server -b 0.0.0.0
