9161624ed476802ea3bb839482ffc6d1f1872d80
Robert Auer <robert.auer@triology.de>
2016-01-29 09:09:57 +0100

    Redmine inserts data into mysql database; #20

--- containers/redmine/docker/startup.sh
+++ containers/redmine/docker/startup.sh
@@ -4,6 +4,9 @@ source /etc/ces/functions.sh
 # install redmine database
 #MYSQL_ROOT_PASSWORD=$(create_or_get_ces_pass mysql_root)
 
+# wait for mysql database to initialize
+#TODO: do not sleep, but check if mysql is available instead
+sleep 60
 
 # get variables for templates
 FQDN=$(get_fqdn)
@@ -26,6 +29,20 @@ fi
 # adjust redmine database.yml
 sed -i "s/\"\"/${MYSQL_USER_PASSWORD}/g" /usr/src/redmine/config/database.yml
 
+# generate secret session token
+bundle exec rake generate_secret_token
+
+# insert secret_key_base into secrets.yml
+sed -i s/X/$(grep secret_key_base /usr/src/redmine/config/initializers/secret_token.rb | awk -F \' '{print $2}' )/g /usr/src/redmine/config/secrets.yml
+
+# Create the database structure
+echo "creating database structure..."
+RAILS_ENV=production bundle exec rake db:migrate
+
+# Insert default configuration data in database
+# TODO: set correct language at REDMINE_LANG parameter
+echo "inserting default configuration data into database"
+RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
 
 # start redmine
 rails server -b 0.0.0.0
