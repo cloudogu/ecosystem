5c813eb7269f6635f26f0a351dff6b96c85a3991
Michael Behlendorf <michael.behlendorf@triology.de>
2016-02-02 09:50:42 +0100

    Improved scm-cas-plugin installation #26

--- containers/scm/docker/startup.sh
+++ containers/scm/docker/startup.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 source /etc/ces/functions.sh
-echo "1"
 if ! [ -d /var/lib/scm/plugins/de/triology/scm/plugins/scm-cas-plugin ]
 	then
-		echo "No cas-plugin"
+		echo "no cas-plugin installed"
 		/opt/scm-server/bin/scm-server &
-		FQDN=$(get_fqdn)
+		START_TIME=$SECONDS
+		tries=0
 		while ! [ $(curl -sL -w "%{http_code}" "http://localhost:8080/scm/api/rest/plugins/installed" -u scmadmin:scmadmin -o /dev/null) -eq 200 ]
 		do
+			((tries++))
 			echo "wait for scm"
 			sleep 1
+			if [ $tries -gt 200 ]
+				then
+					echo "scm didnt start"
+					echo "exit now"
+					kill $!
+					exit 1
+			fi
 		done
-		echo "Install scm-cas-plugin"
+		echo "install scm-cas-plugin"
 		/usr/bin/curl -X POST http://localhost:8080/scm/api/rest/plugins/install/de.triology.scm.plugins:scm-cas-plugin:1.7 -u scmadmin:scmadmin
-		mkdir "/var/lib/scm/config"
-	  render_template "/opt/scm-server/conf/cas_plugin.xml.tpl" > "/var/lib/scm/config/cas_plugin.xml"
-		echo "2"
+
 		kill $!
 fi
-echo "3"
+FQDN=$(get_fqdn)
+mkdir "/var/lib/scm/config"
+render_template "/opt/scm-server/conf/cas_plugin.xml.tpl" > "/var/lib/scm/config/cas_plugin.xml"
 exec /opt/scm-server/bin/scm-server
