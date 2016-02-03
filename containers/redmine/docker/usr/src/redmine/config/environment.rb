69059ead603d85b5e8731982230b19036c73946d
Robert Auer <robert.auer@triology.de>
2016-02-03 11:15:42 +0100

    Added environment.rb; #20

---
+++ containers/redmine/docker/usr/src/redmine/config/environment.rb
@@ -0,0 +1,17 @@
+# Load the Rails application
+require File.expand_path('../application', __FILE__)
+
+# Make sure there's no plugin in vendor/plugin before starting
+vendor_plugins_dir = File.join(Rails.root, "vendor", "plugins")
+if Dir.glob(File.join(vendor_plugins_dir, "*")).any?
+  $stderr.puts "Plugins in vendor/plugins (#{vendor_plugins_dir}) are no longer allowed. " +
+    "Please, put your Redmine plugins in the `plugins` directory at the root of your " +
+    "Redmine directory (#{File.join(Rails.root, "plugins")})"
+  exit 1
+end
+
+# Initialize the Rails application
+Rails.application.initialize!
+#ActionController::AbstractRequest.relative_url_root = "/redmine"
+Redmine::Utils::relative_url_root = "/redmine"
+ActionController::Base.relative_url_root = "/redmine"
