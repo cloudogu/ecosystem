c6a9bac6df7d89c6ddba363e3ba2db4359ca842f
Sebastian Sdorra <sebastian.sdorra@triology.de>
2016-02-17 09:02:16 +0100

    improve nginx container build

--- containers/nginx/docker/build.sh
+++ containers/nginx/docker/build.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-exec apk --update add openssl-dev pcre-dev zlib-dev wget build-base && \
-    mkdir -p /tmp/src && \
-    cd /tmp/src && \
-    wget http://nginx.org/download/${NGINX_VERSION}.tar.gz && \
-    tar -zxvf ${NGINX_VERSION}.tar.gz && \
-    cd /tmp/src/${NGINX_VERSION} && \
-    ./configure \
-        --with-http_ssl_module \
-        --with-http_gzip_static_module \
-        --with-http_sub_module \
-        --with-http_v2_module \
-#        --prefix=/etc/nginx \
-        --http-log-path=/var/log/nginx/access.log \
-        --error-log-path=/var/log/nginx/error.log \
-        --sbin-path=/usr/sbin/nginx \
-        --prefix=/resources && \
-    make
+#!/bin/bash -e
+./configure \
+    --with-http_ssl_module \
+    --with-http_gzip_static_module \
+    --with-http_sub_module \
+    --with-http_v2_module \
+    --prefix=/etc/nginx \
+    --http-log-path=/var/log/nginx/access.log \
+    --error-log-path=/var/log/nginx/error.log \
+    --sbin-path=/usr/sbin/nginx
+make
+make install
+
+# move binary
+mv /usr/sbin/nginx /dist
