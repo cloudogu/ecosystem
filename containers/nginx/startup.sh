5f063f85b3bcea716523782606ab3786e59b1180
Sebastian Sdorra <sebastian.sdorra@triology.de>
2016-03-31 10:37:58 +0200

    move generated certificates to etcd and generate java keystores on application startup

--- containers/nginx/startup.sh
+++ containers/nginx/startup.sh
@@ -6,6 +6,10 @@ echo "[nginx] booting container. ETCD: $ETCD"
 ETCD=http://$(cat /etc/ces/node_master):4001
 FQDN=$(get_fqdn)
 
+# get certificates
+etcdctl --peers "$(cat /etc/ces/node_master):4001" get /config/_global/certificate/server.crt > "/etc/ssl/server.crt"
+etcdctl --peers "$(cat /etc/ces/node_master):4001" get /config/_global/certificate/server.key > "/etc/ssl/server.key"
+
 # include fqdn in ssl.conf
 render_template "/etc/nginx/include.d/ssl.conf.tpl" > "/etc/nginx/include.d/ssl.conf"
 
