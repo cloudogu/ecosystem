25e0b494eb71f8145bb3029878271d654922d3e4
mbehlendorf <michael.behlendorf@triology.de>
2016-04-05 11:17:39 +0200

    added redmine base url #99

--- containers/sonar/resources/sonarbaseurl.sh
+++ containers/sonar/resources/sonarbaseurl.sh
@@ -17,4 +17,3 @@ do
 done
 
 curl 'http://127.0.0.1:9000/sonar/settings/update?category=general&subcategory=general' -H 'content-type: application/x-www-form-urlencoded' --data "settings%5Bsonar.core.serverBaseURL%5D=https://$FQDN/sonar/&page_version=1" -u "admin":"admin"
-#settings%5Bsonar.links.ci%5D=&settings%5Bsonar.ce.maxLogsPerTask%5D=&settings%5Bsonar.preview.includePlugins%5D=&settings%5Bsonar.preview.excludePlugins%5D=&settings%5Bsonar.links.scm%5D=&
