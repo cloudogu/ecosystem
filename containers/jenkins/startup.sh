95f5bde172ec44bf880b53f726f98e713e266b91
mbehlendorf <michael.behlendorf@triology.de>
2016-04-18 15:05:58 +0200

    removed temporary files #107

--- containers/jenkins/startup.sh
+++ containers/jenkins/startup.sh
@@ -8,9 +8,9 @@ RELAYHOST="postfix"
 
 if [ ! -f ${JENKINS_HOME}/config.xml ]; then
 	# render template
-	render_template "/config.xml.tpl" > /var/lib/jenkins/config.xml
-	render_template "/hudson.tasks.Mailer.xml.tpl" > "/var/lib/jenkins/hudson.tasks.Mailer.xml"
-	render_template "/jenkins.model.JenkinsLocationConfiguration.xml.tpl" > "/var/lib/jenkins/jenkins.model.JenkinsLocationConfiguration.xml"
+	render_template "/var/tmp/resources/config.xml.tpl" > /var/lib/jenkins/config.xml
+	render_template "/var/tmp/resources/hudson.tasks.Mailer.xml.tpl" > "/var/lib/jenkins/hudson.tasks.Mailer.xml"
+	render_template "/var/tmp/resources/jenkins.model.JenkinsLocationConfiguration.xml.tpl" > "/var/lib/jenkins/jenkins.model.JenkinsLocationConfiguration.xml"
 
 else
 	# refresh cas IP
@@ -22,7 +22,7 @@ if [ ! -f /var/lib/jenkins/plugins/cas-plugin.hpi ]; then
 	# Making directory, if not already existing
 	mkdir -p /var/lib/jenkins/plugins
 	# Copy plugin
-        cp /cas-plugin.hpi /var/lib/jenkins/plugins/
+        mv /var/tmp/cas-plugin.hpi /var/lib/jenkins/plugins/
 fi
 
 # generating ssh key for usage in slaves and deployment in etcd
@@ -36,6 +36,10 @@ set_config pubkey "$(cat /var/lib/jenkins/.ssh/id_rsa.pub)"
 # create truststore
 TRUSTSTORE="/var/lib/jenkins/truststore.jks"
 create_truststore.sh "${TRUSTSTORE}" > /dev/null
+
+# remove resources
+rm -rf /var/tmp/resources
+
 
 # starting jenkins
 java -Djava.awt.headless=true \
