4758438163d7e6e93076ec3782cecea36d954a0b
Robert Auer <robert.auer@triology.de>
2016-04-27 15:56:09 +0200

    Update node_master to new IP; script for update of fqdn works, but not via the ipchangecheck upstart method; #23

--- resources/etc/ipchange.sh
+++ resources/etc/ipchange.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 source /etc/ces/functions.sh
-#  # Check if system has got a new IP after reboot
+
 CURRIP=$(get_ip)
 LASTIP=$(cat /etc/lastIP)
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+# Check if system has got a new IP after reboot
 if [ "${LASTIP}" != "${CURRIP}" ]; then
-  # IP changed, reinstall certificates
+  # IP changed
+  echo ${CURRIP} > /etc/ces/node_master
+  if valid_ip ${CURRIP}; then set_config_global fqdn ${CURRIP}; fi
+  # Reinstall certificates
   $INSTALL_HOME/install/ssl.sh
   # Save current IP address
   echo $CURRIP > /etc/lastIP
