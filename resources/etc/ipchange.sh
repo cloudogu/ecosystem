fb4eb24e88988cfd7484fe215b4eed57d6ac9420
Robert Auer <robert.auer@triology.de>
2016-05-04 14:32:12 +0200

    Removed debug output

--- resources/etc/ipchange.sh
+++ resources/etc/ipchange.sh
@@ -3,7 +3,6 @@ source /etc/ces/functions.sh
 
 CURRIP=$(get_ip)
 LASTIP=$(cat /etc/lastIP)
-LOGFILE=/etc/ipchange.log
 
 function valid_ip()
 {
@@ -26,27 +25,17 @@ function valid_ip()
 if [ "${LASTIP}" != "${CURRIP}" ]; then
   # IP changed
   echo ${CURRIP} > /etc/ces/node_master
-  echo "ip nach node_master? $?" >> ${LOGFILE}
   if $(valid_ip ${CURRIP}) ; then
-    echo "${CURRIP} is a valid IP" >> ${LOGFILE}
     /opt/ces/bin/etcdctl --peers $(cat /etc/ces/node_master):4001 set "/config/_global/fqdn" "${CURRIP}"
     ETCDCTL_EXIT=$?
-    echo "etcdctl command set fqdn? ${ETCDCTL_EXIT}" >> ${LOGFILE}
     while [ "${ETCDCTL_EXIT}" -ne "0" ]; do # etcd is not ready yet
       sleep 2
-      echo "sleep 2? $?" >> ${LOGFILE}
       /opt/ces/bin/etcdctl --peers $(cat /etc/ces/node_master):4001 set "/config/_global/fqdn" "${CURRIP}"
       ETCDCTL_EXIT=$?
-      echo "etcdctl command set fqdn? ${ETCDCTL_EXIT}" >> ${LOGFILE}
     done
-  else
-    echo "${CURRIP} is no valid IP" >> ${LOGFILE}
   fi
   # Reinstall certificates
   source /etc/environment; ${INSTALL_HOME}/install/ssl.sh
-  echo "ssl-sh? $?" >> ${LOGFILE}
   # Save current IP address
   echo $CURRIP > /etc/lastIP
-  echo "ip nach lastIP? $?" >> ${LOGFILE}
-  echo "" >> ${LOGFILE}
 fi
