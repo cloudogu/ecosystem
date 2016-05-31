0525a03c9e1186af44f63de8af8574948e3e8e53
Thomas Grosser <thomas.grosser@triology.de>
2016-05-31 09:37:21 +0200

    used static linking

--- containers/icoordinator/src/icoordinator.go
+++ containers/icoordinator/src/icoordinator.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 
 func main() {
-    r := gin.Default()
-    r.GET("/icoordinator", func(c *gin.Context) {
-      c.Redirect(http.StatusMovedPermanently, "https://apps.icoordinator.com/")
-    })
-    r.Run(":80")
+	r := gin.Default()
+	r.GET("/icoordinator", func(c *gin.Context) {
+		c.Redirect(http.StatusTemporaryRedirect, "https://apps.icoordinator.com/")
+	})
+	r.Run(":80")
 }
