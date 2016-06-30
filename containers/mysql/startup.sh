15984969c65feba3785d3d945d33d6b78173591f
Sebastian Sdorra <sebastian.sdorra@triology.de>
2016-06-30 15:11:52 +0200

    mysql: added service-account-create command, implemented state health check and improved startup script

--- containers/mysql/startup.sh
+++ containers/mysql/startup.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 mkdir -p /var/run/mysqld
 chown -R mysql: /var/run/mysqld
+
+# installation
 if [ ! -f /var/lib/mysql/ibdata1 ]; then
+    # set stage for health check
+    doguctl state installing
+
+    # install database
     mysql_install_db --user=mysql --datadir="/var/lib/mysql"
-    /usr/bin/mysqld_safe &
-    sleep 10s
-    source /etc/ces/functions.sh
-    MYSQL_ROOT_PASSWORD=$(create_or_get_ces_pass mysql_root)
-    # http://www.jbmurphy.com/2011/06/20/mysql_secure_installation/
+
+    # start daemon in background
+    /usr/bin/mysqld_safe --user=mysql &
+
+    # create random root password
+    MYSQL_ROOT_PASSWORD=$(doguctl random)
+
+    # store the password encrypted
+    doguctl config password -e "${MYSQL_ROOT_PASSWORD}"
+
+    # wait until mysql is ready to accept connections
+    doguctl wait --port 3306
+
+    # set generated root password
     mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY \"${MYSQL_ROOT_PASSWORD}\" WITH GRANT OPTION;"
+
+    # secure the installation
+    # http://www.jbmurphy.com/2011/06/20/mysql_secure_installation/
     mysql -uroot -e "DROP DATABASE test;"
     mysql -uroot -e "DELETE FROM mysql.user WHERE User=\";\""
     mysql -uroot -e "FLUSH PRIVILEGES;"
-    sleep 2
-    killall mysqld
-    sleep 10s
-fi
 
-/usr/bin/mysqld_safe --user=mysql
+    # set stage for health check
+    doguctl state ready
+
+    # reattach daemon
+    wait
+else
+  # set stage for health check
+  doguctl state ready
+
+  # start mysql
+  exec /usr/bin/mysqld_safe --user=mysql
+fi
