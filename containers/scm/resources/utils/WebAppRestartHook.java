0fee3761583d952c2791cb91638387289fb7725b
Maltek709 <maltekreutzfeldt@web.de>
2016-07-06 10:14:15 +0200

    changed @triology.de to @cloudogu.com

--- containers/scm/resources/utils/WebAppRestartHook.java
+++ containers/scm/resources/utils/WebAppRestartHook.java
@@ -1,10 +1,10 @@
 /***
  * Copyright (c) 2015, Sebastian Sdorra
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -13,7 +13,7 @@
  * 3. Neither the name of SCM-Manager; nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -24,9 +24,9 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * https://bitbucket.org/sdorra/scm-manager
- * 
+ *
  */
 package sonia.scm.ces;
 
@@ -36,13 +36,13 @@ import javax.management.*;
 
 /**
  * Hook to restart a jetty web application via jmx.
- * 
- * @author Sebastian Sdorra <sebastian.sdorra@triology.de>
+ *
+ * @author Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
  */
 public final class WebAppRestartHook {
 
   private static final String ONAME = "org.eclipse.jetty.webapp:type=webappcontext,name=%s,id=%s";
-  
+
   public static void restart(String name, String id) throws Exception {
     String webapp = String.format(ONAME, name, id);
 
@@ -51,5 +51,5 @@ public final class WebAppRestartHook {
     server.invoke(oname, "stop", new String[0], new String[0]);
     server.invoke(oname, "start", new String[0], new String[0]);
   }
-  
+
 }
