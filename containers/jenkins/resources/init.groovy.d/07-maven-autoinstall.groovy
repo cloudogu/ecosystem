e3ae87d17d3f2228b0d13bdc0dd427cf7bd223b0
Robert Auer <robert.auer@cloudogu.com>
2016-07-11 10:07:22 +0200

    If Maven 3.3.9 is not installed already, it will be via 07-maven-autoinstall.groovy script

--- containers/jenkins/resources/init.groovy.d/07-maven-autoinstall.groovy
+++ containers/jenkins/resources/init.groovy.d/07-maven-autoinstall.groovy
@@ -7,10 +7,28 @@ import hudson.tools.*
 def instance = Jenkins.getInstance();
 
 def mavenDesc = instance.getDescriptor("hudson.tasks.Maven");
-def mvInstaller = new Maven.MavenInstaller("3.3.9");
-def instSourcProp = new InstallSourceProperty([mvInstaller]);
-def mavenInst = new Maven.MavenInstallation("Maven_3.3.9", null, [instSourcProp]);
-mavenDesc.setInstallations(mavenInst);
-mavenDesc.save();
-
-instance.save();
+def installations = mavenDesc.getInstallations();
+println "DEBUG: Existing Maven installations:";
+boolean mavenInstalled = false;
+if (installations != null) {
+  installations.each { installation ->
+    println "DEBUG: installation name: ${installation}";
+    def inst = "${installation}"
+    if (inst.contains("Maven_3.3.9")) {
+      println "DEBUG: Maven is already installed";
+      mavenInstalled = true;
+    } else {
+      println "DEBUG: That's not Maven";
+    }
+  }
+} else {
+  println "DEBUG: no maven installations";
+}
+if (!mavenInstalled) {
+  def mvInstaller = new Maven.MavenInstaller("3.3.9");
+  def instSourcProp = new InstallSourceProperty([mvInstaller]);
+  def mavenInst = new Maven.MavenInstallation("Maven_3.3.9", null, [instSourcProp]);
+  mavenDesc.setInstallations(mavenInst);
+  mavenDesc.save();
+  instance.save();
+}
