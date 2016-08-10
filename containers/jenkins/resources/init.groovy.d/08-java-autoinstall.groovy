e8739dd73060714114bd0c9ab6eb479016a4c371
Robert Auer <robert.auer@cloudogu.com>
2016-08-10 19:37:26 +0200

    Removed update of updatecenter in script; #180

--- containers/jenkins/resources/init.groovy.d/08-java-autoinstall.groovy
+++ containers/jenkins/resources/init.groovy.d/08-java-autoinstall.groovy
@@ -6,7 +6,6 @@ import jenkins.model.Jenkins
 def descriptor = new JDK.DescriptorImpl();
 
 if (!descriptor.getInstallations()) {
-    Jenkins.instance.updateCenter.getById('default').updateDirectlyNow(true)
     def jdkInstaller = new JDKInstaller('jdk-8u102-oth-JPR', true)
     def jdk = new JDK("jdk8u102", null, [new InstallSourceProperty([jdkInstaller])])
     descriptor.setInstallations(jdk)
