de8333d74913686e113a66ffe50059f53b7f2b50
Robert Auer <robert.auer@cloudogu.com>
2016-08-15 14:43:59 +0200

    Updated version in dogu.json; added subversion plugin; #170

--- containers/jenkins/resources/init.groovy.d/init01installplugins.groovy
+++ containers/jenkins/resources/init.groovy.d/init01installplugins.groovy
@@ -14,7 +14,7 @@ def keyExists(String key){
 }
 
 // configuration
-def plugins = ['git','mercurial','workflow-aggregator','simple-theme-plugin','maven-plugin'];
+def plugins = ['git','mercurial','workflow-aggregator','simple-theme-plugin','maven-plugin','subversion'];
 
 // add sonar plugin to Jenkins if SonarQube is installed
 if (keyExists("dogu/sonar/current")) {
