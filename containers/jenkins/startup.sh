926bec1f4c5bfdfe8276aec00a6453edcb88d376
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2016-08-17 09:35:14 +0200

    configure git, mercurial and subversion to use the ca certificate from the registry #130
    The base image was also update in order to use java 8u102 and Jenkins was updated to version 1.651.3

--- containers/jenkins/startup.sh
+++ containers/jenkins/startup.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 source /etc/ces/functions.sh
 
-# Checking if /var/lib/jenkins/cas-plugin exists
-if [ ! -f /var/lib/jenkins/plugins/cas-plugin.hpi ]; then
-	# Making directory, if not already existing
-	mkdir -p /var/lib/jenkins/plugins
-	# Copy plugin
-  cp /var/tmp/cas-plugin.hpi /var/lib/jenkins/plugins/
-fi
+### functions
 
-# create truststore
+function copy_cas_plugin() {
+  # Checking if /var/lib/jenkins/cas-plugin exists
+  if [ ! -f /var/lib/jenkins/plugins/cas-plugin.hpi ]; then
+  	# Making directory, if not already existing
+  	mkdir -p /var/lib/jenkins/plugins
+  	# Copy plugin
+    cp /var/tmp/cas-plugin.hpi /var/lib/jenkins/plugins/
+  fi
+}
+
+### functions end
+
+# copy custom jenkins cas plugin
+copy_cas_plugin
+
+# create truststore for java processes
 TRUSTSTORE="/var/lib/jenkins/truststore.jks"
-create_truststore.sh "${TRUSTSTORE}" > /dev/null
+create_truststore.sh /var/lib/jenkins/truststore.jks > /dev/null
+
+# create ca store for git, mercurial and subversion
+create-ca-certificates.sh /var/lib/jenkins/ca-certificates.crt
 
 # copy init scripts
 cp -rf /var/tmp/resources/init.groovy.d /var/lib/jenkins/
@@ -20,5 +32,5 @@ cp -rf /var/tmp/resources/init.groovy.d /var/lib/jenkins/
 java -Djava.awt.headless=true \
   -Djava.net.preferIPv4Stack=true \
   -Djavax.net.ssl.trustStore="${TRUSTSTORE}" \
-	-Djavax.net.ssl.trustStorePassword=changeit \
+  -Djavax.net.ssl.trustStorePassword=changeit \
   -jar /jenkins.war --prefix=/jenkins
