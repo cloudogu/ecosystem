fb5be4efdfe35cc1b2536ccd0ad46962d267ec86
Robert Auer <robert.auer@cloudogu.com>
2016-11-01 09:12:10 +0100

    Removed copied basebox scripts

--- image/scripts/dep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Setup the the box. This runs as root
-#apt-get -y update
-#apt-get -y install htop iftop jq ruby ruby-dev
-# You can install anything you need here.
-echo "installing docker"
-curl -sSL https://get.docker.com/ | sh
-echo "install docker - end"
