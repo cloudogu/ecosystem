7c1949a70ad3ada1d140cb71f2929c6baefc5c6c
Robert Auer <robert.auer@cloudogu.com>
2016-11-01 09:17:21 +0100

    Added customization script

--- image/scripts/subvolumes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "creating btrfs subvolumes"
-sudo btrfs subvolume create /var/lib/docker
-sudo btrfs subvolume create /var/lib/ces
-#sudo btrfs subvolume create /opt/ces
-echo "creating btrfs subvolumes - end"
