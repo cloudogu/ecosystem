1cf7ea7bcbc4931a7971b5ca04d0607c1870eca0
Robert Auer <robert.auer@cloudogu.com>
2016-11-02 12:51:00 +0100

    Changed script to install fixed version of docker

--- images/scripts/dep.sh
+++ images/scripts/dep.sh
@@ -3,7 +3,16 @@
 # Setup the the box. This runs as root
 apt-get -y update
 apt-get -y install htop iftop jq ruby ruby-dev
-# You can install anything you need here.
+
+# Install docker
+# see https://docs.docker.com/engine/installation/linux/ubuntulinux/
+
+DOCKER_VERSION=1.12.3-0~trusty
+
 echo "installing docker"
-curl -sSL https://get.docker.com/ | sh
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list
+apt-get -y update
+apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get -y install docker-engine=$DOCKER_VERSION
 echo "install docker - end"
