85017aec5cb43d9acfe80ede16133c250ec71651
Stephan Christann <stephan.christann@christann.net>
2016-12-02 15:40:48 +0100

    tested vmware tools installation and integration

--- images/scripts/commons/vmwareTools.sh
+++ images/scripts/commons/vmwareTools.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Make sure perl is available
-yum -y install perl
-
-# Mount the disk image
-cd /tmp
-mkdir /tmp/isomount
-mount -t iso9660 -o loop /root/linux.iso /tmp/isomount
-
-# Install the drivers
-cp /tmp/isomount/VMwareTools-*.gz /tmp
-tar -zxvf VMwareTools*.gz
-./vmware-tools-distrib/vmware-install.pl -d
-
-# Cleanup
-umount isomount
-rm -rf isomount /root/linux.iso VMwareTools*.gz vmware-tools-distrib
+DEBIAN_FRONTEND=noninteractive apt-get -y install \
+  open-vm-tools
