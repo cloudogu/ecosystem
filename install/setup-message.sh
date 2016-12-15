14805b01c9c42b13931a281dafb475927f38cb63
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2016-12-15 09:30:26 +0100

    changed setup message

--- install/setup-message.sh
+++ install/setup-message.sh
@@ -6,10 +6,6 @@ source /etc/ces/functions.sh
 source /etc/environment
 export PATH
 
-if [ -f "/vagrant/setup.json" ]; then
-  echo "The unattenden setup has started with instruction from /vagrant/setup.json"
-else
-  URL=http://$(get_ip):8080
-  echo "the setup daemon has started and can be accessed at $URL"
-fi
+URL=http://$(get_ip):8080
+echo "the setup daemon has started and can be accessed at $URL"
 echo "The setup runs in background and writes its logs to /var/log/upstart/ces-setup.log"
