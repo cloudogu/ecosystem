c11ee8df9021fb1f6274e93f74622c03b0d4bdaa
Robert Auer <robert.auer@cloudogu.com>
2017-01-17 14:10:10 +0100

    Removed old code

--- containers/nexus/startup.sh
+++ containers/nexus/startup.sh
@@ -31,36 +31,12 @@ if ! [ -e /var/lib/nexus/conf/security-configuration.xml ]; then
   cp /lvo-plugin.xml /var/lib/nexus/conf/lvo-plugin.xml
 fi
 
-if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-${CAS_PLUGIN_VERSION} ]
-  then
-      echo "No cas-plugin installed"
-      $START_NEXUS &
-      tries=0
-  		while ! [ $(curl -sL -w "%{http_code}" "http://localhost:8081/nexus" -u $ADMUSR:$ADMPW -o /dev/null) -eq 200 ]
-  		do
-  			((tries++))
-  			echo "wait for nexus (cas-plugin)"
-  			sleep 1
-  			if [ $tries -gt 200 ]
-  				then
-  					echo "nexus didnt start"
-  					echo "exit now"
-  					kill $!
-  					exit 1
-  			fi
-  	  done
-      # add Cas Plugin
-      cp -a /opt/sonatype/nexus/resources/nexus-cas-plugin-${CAS_PLUGIN_VERSION}/ /var/lib/nexus/plugin-repository/
-      # add mailconfig
-#      mailConfiguration=$(curl -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW" | jq ".data.smtpSettings+={\"host\": \"postfix\"}" | jq ".data.smtpSettings+={\"username\": \"\"}" | jq ".data.smtpSettings+={\"password\": \"\"}" | jq ".data.globalRestApiSettings+={\"baseUrl\": \"https://$FQDN/nexus/\"}" | jq ".data.smtpSettings+={\"systemEmailAddress\": \"nexus@$DOMAIN\"}" | jq ".data+={\"securityAnonymousAccessEnabled\": false}" | jq ".data+={\"securityRealms\": [\"XmlAuthenticatingRealm\",\"XmlAuthorizingRealm\",\"CasAuthenticatingRealm\"]}")
-      echo "============ CONFIG INFO ============"
-#      echo $mailConfiguration | jq .
-#      curl -H "Content-Type: application/json" -X PUT -d "$mailConfiguration" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
-      # disable new version info
-#      curl -H "Content-Type: application/json" -X PUT -d "{"data":{"enabled":false}}" "http://127.0.0.1:8081/nexus/service/local/lvo_config" -u "$ADMUSR":"$ADMPW"
-      echo "========== CONFIG INFO END =========="
-      kill $!
+# Install cas-plugin
+if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-${CAS_PLUGIN_VERSION} ]; then
+  mkdir -p /var/lib/nexus/plugin-repository
+  cp -a /opt/sonatype/nexus/resources/nexus-cas-plugin-${CAS_PLUGIN_VERSION}/ /var/lib/nexus/plugin-repository/
 fi
+
 FQDN=$(get_fqdn)
 echo "render_template"
 # update cas url
