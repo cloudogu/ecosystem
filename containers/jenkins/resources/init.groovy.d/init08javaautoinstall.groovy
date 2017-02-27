a7f7718a3185240539a94697181c1c12b2630e06
Robert Auer <robert.auer@cloudogu.com>
2017-02-27 15:29:21 +0100

    Jenkins auto-installation is called OracleJDK8 now; #245

--- containers/jenkins/resources/init.groovy.d/init08javaautoinstall.groovy
+++ containers/jenkins/resources/init.groovy.d/init08javaautoinstall.groovy
@@ -7,6 +7,6 @@ def descriptor = new JDK.DescriptorImpl();
 
 if (!descriptor.getInstallations()) {
     def jdkInstaller = new JDKInstaller('jdk-8u121-oth-JPR', true)
-    def jdk = new JDK("jdk8u121", null, [new InstallSourceProperty([jdkInstaller])])
+    def jdk = new JDK("OracleJDK8", null, [new InstallSourceProperty([jdkInstaller])])
     descriptor.setInstallations(jdk)
 }
