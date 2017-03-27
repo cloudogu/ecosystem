687021568c03141c4e7cc39a37c351b119887038
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-27 16:52:43 +0200

    support of multiple updateSites #259

--- containers/jenkins/resources/init.groovy.d/init10configureUpdateSite.groovy
+++ containers/jenkins/resources/init.groovy.d/init10configureUpdateSite.groovy
@@ -2,27 +2,42 @@ import jenkins.model.*;
 import hudson.model.*
 import groovy.json.JsonSlurper;
 
-def getValueFromEtcd(String key){
+def getValuesFromEtcd(String key){
 	String ip = new File("/etc/ces/node_master").getText("UTF-8").trim();
 	URL url = new URL("http://${ip}:4001/v2/keys/${key}");
-	def json = new JsonSlurper().parseText(url.text)
-	return json.node.value
+	def json = new JsonSlurper().parseText(url.text);
+	println json;
+	return convertNodesToUpdateSites(json.node.nodes, json.node.key.length());
+}
+
+def convertNodesToUpdateSites(Object nodes, int keyOffset) {
+	println "convertNodesToUpdateSites";
+	List<hudson.model.UpdateSite> updateSites = [];
+	nodes.each{ node ->
+		def name = node.key.substring(keyOffset+1);
+		def url = node.value;
+		println "UpdateSite: "+name+" "+url;
+		site = new hudson.model.UpdateSite(name,url);
+		updateSites.add(site);
+	}
+	return updateSites;
 }
 
 def instance = Jenkins.getInstance();
 
-String updateSiteUrl = getValueFromEtcd("config/jenkins/updateSiteUrl");
+List<hudson.model.UpdateSite> updateSites = getValuesFromEtcd("config/jenkins/updateSiteUrl");
 
 // TODO: check if already done?
-if(updateSiteUrl != null && !updateSiteUrl.isEmpty()) {
+if(updateSites.size() > 0) {
 	println "set signatureCheck=false";
 	hudson.model.DownloadService.signatureCheck = false;
 	updateCenter = instance.getUpdateCenter();
 	println "change Update Site URL";
-	def customUpdateSite = new hudson.model.UpdateSite("default", updateSiteUrl)
 	def sites = updateCenter.getSites();
-	sites.clear()
-    sites.add(customUpdateSite);
-	println "save instance"
+	sites.clear();
+	for(hudson.model.UpdateSite site : updateSites) {
+		sites.add(site);
+	}
+	println "save instance";
 	instance.save();
 }
