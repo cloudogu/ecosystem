3f3f26045fabcf1385b298a4e7386e27d580a471
Robert Auer <robert.auer@cloudogu.com>
2017-03-29 09:11:30 +0200

    Remove javadocs dogu

--- containers/javadocs/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
-
-# Start nginx
-echo "[nginx] starting nginx service..."
-exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
