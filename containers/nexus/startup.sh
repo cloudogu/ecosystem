7c4f6789e0cce4705b3ff48c54012b29c54fd90e
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-30 16:43:23 +0200

    accelerate nexus startup

--- containers/nexus/startup.sh
+++ containers/nexus/startup.sh
@@ -8,6 +8,13 @@ ADMINGROUP=$(doguctl config --global admin_group)
 DOMAIN=$(doguctl config --global domain)
 FQDN=$(doguctl config --global fqdn)
 
+function set_random_admin_password {
+  ADMPW=$(doguctl random)
+  curl --retry 3 --retry-delay 10 -X POST -H "Content-Type: application/json" -d "{data:{userId:\"$ADMUSR\",newPassword:\"$ADMPW\"}}" --insecure 'http://127.0.0.1:8081/nexus/service/local/users_setpw' -u "$ADMUSR":"$ADMDEFAULTPW"
+  doguctl config -e "admin_password" "${ADMPW}"
+  echo "${ADMPW}"
+}
+
 # create truststore
 TRUSTSTORE="/var/lib/nexus/truststore.jks"
 create_truststore.sh "${TRUSTSTORE}" > /dev/null
@@ -20,19 +27,6 @@ START_NEXUS="java \
   -cp conf/:`(echo lib/*.jar) | sed -e "s/ /:/g"` \
   org.sonatype.nexus.bootstrap.Launcher ./conf/jetty.xml ./conf/jetty-requestlog.xml"
 
-if [ "$(doguctl config -e "admin_password")" ]; then
-  echo "Set new admin password"
-  ADMPW=$(doguctl random)
-  $START_NEXUS &
-  if ! doguctl wait --port 8081 --timeout 120; then 
-    echo "Nexus seems not to be started. Exiting."
-    exit 1
-  fi
-  curl --retry 3 --retry-delay 10 -X POST -H "Content-Type: application/json" -d "{data:{userId:\"$ADMUSR\",newPassword:\"$ADMPW\"}}" --insecure 'http://127.0.0.1:8081/nexus/service/local/users_setpw' -u "$ADMUSR":"$ADMDEFAULTPW"
-  doguctl config -e "admin_password" "${ADMPW}"
-  kill $!
-fi
-ADMPW=$(doguctl config -e "admin_password")
 
 if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-"${CAS_PLUGIN_VERSION}" ]; then
       echo "No cas-plugin installed"
@@ -42,6 +36,7 @@ if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-"${CAS_PLUGIN_VERSIO
         echo "Nexus seems not to be started. Exiting."
         exit 1
       fi
+      ADMPW=$(set_random_admin_password)
   		
       # add Cas Plugin
       cp -a /opt/sonatype/nexus/resources/nexus-cas-plugin-"${CAS_PLUGIN_VERSION}"/ /var/lib/nexus/plugin-repository/
@@ -55,6 +50,18 @@ if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-"${CAS_PLUGIN_VERSIO
       echo "========== CONFIG INFO END =========="
       kill $!
 fi
+
+if  ! doguctl config -e "admin_password" > /dev/null ; then
+  $START_NEXUS &
+  if ! doguctl wait --port 8081 --timeout 120; then 
+    echo "Nexus seems not to be started. Exiting."
+    exit 1
+  fi
+  set_random_admin_password
+  kill $!
+fi
+
+ADMPW=$(doguctl config -e "admin_password")
 FQDN=$(get_fqdn)
 echo "render_template"
 # update cas url
