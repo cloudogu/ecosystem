99ea906ace3f0199881326b5c72eafb876e0ffe9
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-30 15:19:48 +0200

    change password of default user; refactoring

--- containers/nexus/configuration.sh
+++ containers/nexus/configuration.sh
@@ -4,22 +4,15 @@ FQDN=$(get_fqdn)
 ADMUSR="$1"
 ADMPW="$2"
 ADMINGROUP="$3"
-tries=0
-while ! [ $(curl -sL -w "%{http_code}" "http://localhost:8081/nexus" -u $ADMUSR:$ADMPW -o /dev/null) -eq 200 ]
-do
-  ((tries++))
-  echo "wait for nexus (configuration)"
-  sleep 1
-  if [ $tries -gt 200 ]
-    then
-      echo "nexus didnt start"
-      echo "exit now"
-      exit 1
-  fi
-done
+
+if ! doguctl wait --port 8081 --timeout 120; then 
+  echo "Nexus seems not to be started. Exiting."
+  exit 1
+fi
+
 # set ADMINGROUP as nx-admins
 echo "set group $ADMINGROUP as nx-admins"
-curl -s -H "Content-Type: application/json" -X POST -d "{"data":{"id":"$ADMINGROUP","name":"$ADMINGROUP","description":"$ADMINGROUP","sessionTimeout":60,"roles":["nx-admin"],"privileges":[]}}" "http://localhost:8081/nexus/service/local/roles" -u $ADMUSR:$ADMPW
+curl --retry 3 --retry-delay 10 -s -H "Content-Type: application/json" -X POST -d "{data:{id:\"$ADMINGROUP\",name:\"$ADMINGROUP\",description:\"$ADMINGROUP\",sessionTimeout:60,roles:[nx-admin],privileges:[]}}" "http://localhost:8081/nexus/service/local/roles" -u "$ADMUSR":"$ADMPW"
 # update base url
 settings=$(curl -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW" | jq ".data.globalRestApiSettings+={\"baseUrl\": \"https://$FQDN/nexus/\"}")
 curl -H "Content-Type: application/json" -X PUT -d "$settings" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
--- containers/nexus/startup.sh
+++ containers/nexus/startup.sh
@@ -3,50 +3,57 @@ source /etc/ces/functions.sh
 
 # variables
 ADMUSR="admin"
-ADMPW="admin123"
-ADMINGROUP=$(get_config admin_group)
-DOMAIN=$(get_config domain)
-FQDN=$(get_fqdn)
+ADMDEFAULTPW="admin123"
+ADMINGROUP=$(doguctl config --global admin_group)
+DOMAIN=$(doguctl config --global domain)
+FQDN=$(doguctl config --global fqdn)
 
 # create truststore
 TRUSTSTORE="/var/lib/nexus/truststore.jks"
 create_truststore.sh "${TRUSTSTORE}" > /dev/null
 
 START_NEXUS="java \
-  -server -XX:MaxPermSize=192m -Djava.net.preferIPv4Stack=true -Xms256m -Xmx1g \
+  -server -Djava.net.preferIPv4Stack=true -Xms256m -Xmx1g \
   -Djavax.net.ssl.trustStore=${TRUSTSTORE} \
 	-Djavax.net.ssl.trustStorePassword=changeit \
   -Dnexus-work=/var/lib/nexus -Dnexus-webapp-context-path=/nexus \
   -cp conf/:`(echo lib/*.jar) | sed -e "s/ /:/g"` \
   org.sonatype.nexus.bootstrap.Launcher ./conf/jetty.xml ./conf/jetty-requestlog.xml"
 
-if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-${CAS_PLUGIN_VERSION} ]
+if [ "$(doguctl config -e "admin_password")" ] 
+  then
+  echo "Set new admin password"
+  ADMPW=$(doguctl random)
+  $START_NEXUS &
+  if ! doguctl wait --port 8081 --timeout 120; then 
+    echo "Nexus seems not to be started. Exiting."
+    exit 1
+  fi
+  curl --retry 3 --retry-delay 10 -X POST -H "Content-Type: application/json" -d "{data:{userId:\"$ADMUSR\",newPassword:\"$ADMPW\"}}" --insecure 'http://127.0.0.1:8081/nexus/service/local/users_setpw' -u "$ADMUSR":"$ADMDEFAULTPW"
+  doguctl config -e "admin_password" "${ADMPW}"
+  kill $!
+fi
+ADMPW=$(doguctl config -e "admin_password")
+
+if ! [ -d /var/lib/nexus/plugin-repository/nexus-cas-plugin-"${CAS_PLUGIN_VERSION}" ]
   then
       echo "No cas-plugin installed"
       $START_NEXUS &
-      tries=0
-  		while ! [ $(curl -sL -w "%{http_code}" "http://localhost:8081/nexus" -u $ADMUSR:$ADMPW -o /dev/null) -eq 200 ]
-  		do
-  			((tries++))
-  			echo "wait for nexus (cas-plugin)"
-  			sleep 1
-  			if [ $tries -gt 200 ]
-  				then
-  					echo "nexus didnt start"
-  					echo "exit now"
-  					kill $!
-  					exit 1
-  			fi
-  	  done
+      
+      if ! doguctl wait --port 8081 --timeout 120; then 
+        echo "Nexus seems not to be started. Exiting."
+        exit 1
+      fi
+  		
       # add Cas Plugin
-      cp -a /opt/sonatype/nexus/resources/nexus-cas-plugin-${CAS_PLUGIN_VERSION}/ /var/lib/nexus/plugin-repository/
+      cp -a /opt/sonatype/nexus/resources/nexus-cas-plugin-"${CAS_PLUGIN_VERSION}"/ /var/lib/nexus/plugin-repository/
       # add mailconfig
       mailConfiguration=$(curl -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW" | jq ".data.smtpSettings+={\"host\": \"postfix\"}" | jq ".data.smtpSettings+={\"username\": \"\"}" | jq ".data.smtpSettings+={\"password\": \"\"}" | jq ".data.globalRestApiSettings+={\"baseUrl\": \"https://$FQDN/nexus/\"}" | jq ".data.smtpSettings+={\"systemEmailAddress\": \"nexus@$DOMAIN\"}" | jq ".data+={\"securityAnonymousAccessEnabled\": false}" | jq ".data+={\"securityRealms\": [\"XmlAuthenticatingRealm\",\"XmlAuthorizingRealm\",\"CasAuthenticatingRealm\"]}")
       echo "============ CONFIG INFO ============"
-      echo $mailConfiguration | jq .
-      curl -H "Content-Type: application/json" -X PUT -d "$mailConfiguration" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
+      echo "$mailConfiguration" | jq .
+      curl --retry 3 --retry-delay 10 -H "Content-Type: application/json" -X PUT -d "$mailConfiguration" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
       # disable new version info
-      curl -H "Content-Type: application/json" -X PUT -d "{"data":{"enabled":false}}" "http://127.0.0.1:8081/nexus/service/local/lvo_config" -u "$ADMUSR":"$ADMPW"
+      curl -H "Content-Type: application/json" -X PUT -d "{data:{enabled:false}}" "http://127.0.0.1:8081/nexus/service/local/lvo_config" -u "$ADMUSR":"$ADMPW"
       echo "========== CONFIG INFO END =========="
       kill $!
 fi
@@ -54,5 +61,5 @@ FQDN=$(get_fqdn)
 echo "render_template"
 # update cas url
 render_template "/opt/sonatype/nexus/resources/cas-plugin.xml.tpl" > "/var/lib/nexus/conf/cas-plugin.xml"
-/configuration.sh $ADMUSR $ADMPW $ADMINGROUP &
+/configuration.sh "$ADMUSR" "$ADMPW" "$ADMINGROUP" &
 exec $START_NEXUS
