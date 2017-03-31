0e4a14ead4f426b4328166e0cdf0de37830a4b2f
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-03-31 08:32:13 +0200

    enable bash strict mode

--- containers/nexus/configuration.sh
+++ containers/nexus/configuration.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 source /etc/ces/functions.sh
+
 FQDN=$(get_fqdn)
 ADMUSR="$1"
 ADMPW="$2"
@@ -12,7 +17,9 @@ fi
 
 # set ADMINGROUP as nx-admins
 echo "set group $ADMINGROUP as nx-admins"
-curl --retry 3 --retry-delay 10 -s -H "Content-Type: application/json" -X POST -d "{data:{id:\"$ADMINGROUP\",name:\"$ADMINGROUP\",description:\"$ADMINGROUP\",sessionTimeout:60,roles:[nx-admin],privileges:[]}}" "http://localhost:8081/nexus/service/local/roles" -u "$ADMUSR":"$ADMPW"
+if curl --retry 3 --retry-delay 10 -s -H "Content-Type: application/json" -X POST -d "{data:{id:\"$ADMINGROUP\",name:\"$ADMINGROUP\",description:\"$ADMINGROUP\",sessionTimeout:60,roles:[nx-admin],privileges:[]}}" "http://localhost:8081/nexus/service/local/roles" -u "$ADMUSR":"$ADMPW"; then
+  echo "Already set admin group"
+fi
 # update base url
 SETTINGS=$(curl -H 'content-type:application/json' -H 'accept:application/json' 'http://127.0.0.1:8081/nexus/service/local/global_settings/current' -u "$ADMUSR":"$ADMPW" | jq ".data.globalRestApiSettings+={\"baseUrl\": \"https://$FQDN/nexus/\"}")
 curl -H "Content-Type: application/json" -X PUT -d "$SETTINGS" "http://127.0.0.1:8081/nexus/service/local/global_settings/current" -u "$ADMUSR":"$ADMPW"
--- containers/nexus/startup.sh
+++ containers/nexus/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 source /etc/ces/functions.sh
 
 # variables
