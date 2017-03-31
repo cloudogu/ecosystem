1416b794239e9fd75f53f8b6173ca75872ba98fc
Jan Niemann <jan.niemann@cloudogu.com>
2017-03-31 19:07:24 +0200

    move docker back to 1.13.1, disable automatic apt updates/upgrades

--- images/scripts/commons/docker.sh
+++ images/scripts/commons/docker.sh
@@ -6,14 +6,16 @@ set -o pipefail
 # Install docker
 # see https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
-DOCKER_VERSION=17.03.0~ce-0~ubuntu-xenial
+DOCKER_VERSION=1.13.1-0~ubuntu-xenial
+
+export DEBIAN_FRONTEND=noninteractive
 
 echo "installing docker"
-DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
 # update docker repository only
-DEBIAN_FRONTEND=noninteractive apt-get -y update -o Dir::Etc::sourcelist="sources.list.d/docker.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+apt-get -y update -o Dir::Etc::sourcelist="sources.list.d/docker.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 # DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-cache policy docker-engine
-DEBIAN_FRONTEND=noninteractive apt-get -y install docker-engine=$DOCKER_VERSION
+apt-cache policy docker-engine
+apt-get -y install docker-engine=$DOCKER_VERSION
 echo "install docker - end"
