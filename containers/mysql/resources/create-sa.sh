5ea712ed4180ad521887174382c0b3bf6af4bca1
Jan Niemann <jan.niemann@cloudogu.com>
2017-04-03 11:02:19 +0200

    clean up create-sa.sh script

--- containers/mysql/resources/create-sa.sh
+++ containers/mysql/resources/create-sa.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -e
-SERVICE="$1"
-if [ "${SERVICE}" == "" ]; then
-  echo "usage create-sa.sh servicename"
-  exit 1
-fi
 
-# create random schema suffix and password
-SCHEMA="${SERVICE}_$(doguctl random -l 6)"
-PASSWORD=$(doguctl random)
+{
+    SERVICE="$1"
+    if [ X"${SERVICE}" = X"" ]; then
+        echo "usage create-sa.sh servicename"
+        exit 1
+    fi
 
-# create database
-2>/dev/null 1>&2 mysql -u root -e "CREATE DATABASE ${SCHEMA} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+    # create random schema suffix and password
+    SCHEMA="${SERVICE}_$(doguctl random -l 6)"
+    PASSWORD=$(doguctl random)
 
-# grant access for user
-2>/dev/null 1>&2 mysql -u root -e "grant all on ${SCHEMA}.* to \"${SCHEMA}\"@\"%\" identified by \"${PASSWORD}\";FLUSH PRIVILEGES;"
+    # create database
+    mysql -u root -e "CREATE DATABASE ${SCHEMA} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+    
+    # grant access for user
+    mysql -u root -e "grant all on ${SCHEMA}.* to \"${SCHEMA}\"@\"%\" identified by \"${PASSWORD}\";FLUSH PRIVILEGES;"
+} >/dev/null 2>&1
 
 # print details
 echo "database: ${SCHEMA}"
