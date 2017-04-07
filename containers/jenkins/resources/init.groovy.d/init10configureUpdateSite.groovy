3bfdbcec522f4c7c0152fc29921d67d09ed815be
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2017-04-07 08:28:39 +0200

    fix jenkins startup without configure update sites

--- containers/jenkins/resources/init.groovy.d/init10configureUpdateSite.groovy
+++ containers/jenkins/resources/init.groovy.d/init10configureUpdateSite.groovy
@@ -2,11 +2,20 @@ import jenkins.model.*;
 import hudson.model.*
 import groovy.json.JsonSlurper;
 
-def getValuesFromEtcd(String key){
-	String ip = new File("/etc/ces/node_master").getText("UTF-8").trim();
-	URL url = new URL("http://${ip}:4001/v2/keys/${key}");
-	def json = new JsonSlurper().parseText(url.text);
-	return convertNodesToUpdateSites(json.node.nodes, json.node.key.length());
+def getJsonFromEtcd(String key) {
+  String ip = new File("/etc/ces/node_master").getText("UTF-8").trim();
+  URL url = new URL("http://${ip}:4001/v2/keys/${key}");
+  return new JsonSlurper().parseText(url.text);
+}
+
+def getUpdateSitesFromEtcd(String key) {
+	try {
+    def json = getJsonFromEtcd(key);
+    return convertNodesToUpdateSites(json.node.nodes, json.node.key.length());
+	} catch (FileNotFoundException ex) {
+    println "could not find update site configuration in registry"
+    return new ArrayList<hudson.model.UpdateSite>();
+  }
 }
 
 def convertNodesToUpdateSites(Object nodes, int parentKeyOffset) {
@@ -15,16 +24,15 @@ def convertNodesToUpdateSites(Object nodes, int parentKeyOffset) {
 		// trim the directory from the nodes key
 		def name = node.key.substring(parentKeyOffset+1);
 		def url = node.value;
-		println "found update site: "+name+" "+url;
-		site = new hudson.model.UpdateSite(name,url);
-		updateSites.add(site);
+    
+		println "found update site: ${name} ${url}";
+		updateSites.add(new hudson.model.UpdateSite(name, url));
 	}
 	return updateSites;
 }
 
 def instance = Jenkins.getInstance();
-
-List<hudson.model.UpdateSite> updateSites = getValuesFromEtcd("config/jenkins/updateSiteUrl");
+List<hudson.model.UpdateSite> updateSites = getUpdateSitesFromEtcd("config/jenkins/updateSiteUrl");
 
 // TODO: check if already done?
 if(updateSites.size() > 0) {
