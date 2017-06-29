25a394be7c91e4835770bfb46d501c36877c2524
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2017-06-29 14:09:35 +0200

    refactor maven installation and update to maven 3.5.0
    Refactored the Apache Maven installation to be more readable. Update Apache Maven to the latest available version (3.5.0) and set the tool name to M3 to match the Github + Maven pipeline example.

--- containers/jenkins/resources/var/tmp/resources/init.groovy.d/init07mavenautoinstall.groovy
+++ containers/jenkins/resources/var/tmp/resources/init.groovy.d/init07mavenautoinstall.groovy
@@ -1,30 +1,45 @@
-import jenkins.model.*;
+// creates a global tool installer for maven.
+
+import jenkins.model.*
 import hudson.*
 import hudson.model.*
 import hudson.tasks.*
 import hudson.tools.*
 
-def instance = Jenkins.getInstance();
+// the name M3 is chosen, because of the GitHub + Maven Pipeline sample
+def mavenName = "M3"
+def mavenVersion = "3.5.0"
 
-def mavenDesc = instance.getDescriptor("hudson.tasks.Maven");
-def installations = mavenDesc.getInstallations();
-boolean isMavenInstalled = false;
-if (installations != null) {
-  installations.each { installation ->
-    def inst = "${installation}"
-    if (inst.contains("Maven3")) {
-      isMavenInstalled = true;
+def boolean isMavenAlreadyInstalled(def mavenName) {
+  def descriptor = Jenkins.instance.getDescriptor("hudson.tasks.Maven")
+  def installations = descriptor.getInstallations()
+  if (installations != null) {
+    installations.each { installation ->
+      def inst = "${installation}"
+      if (inst.contains(mavenName)) {
+        return true
+      }
     }
   }
+  return false
 }
-if (!isMavenInstalled) {
-  def mvInstaller = new Maven.MavenInstaller("3.3.9");
-  def instSourcProp = new InstallSourceProperty([mvInstaller]);
-  def mavenInst = new Maven.MavenInstallation("Maven3", null, [instSourcProp]);
-  mavenInstallations = instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
-  mavenInstallationsList = (mavenInstallations.installations as List);
-  mavenInstallationsList.add(mavenInst);
-  mavenInstallations.installations=mavenInstallationsList;
+
+def createMavenInstallation(def mavenName, def mavenVersion) { 
+  def mvnInstaller = new Maven.MavenInstaller(mavenVersion)
+  def instSourcProp = new InstallSourceProperty([mvnInstaller])
+  return new Maven.MavenInstallation(mavenName, null, [instSourcProp])
+}
+
+def addMavenToInstallations(def installation) {
+  mavenInstallations = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0]
+  mavenInstallationsList = (mavenInstallations.installations as List)
+  mavenInstallationsList.add(installation)
+  mavenInstallations.installations = mavenInstallationsList
   mavenInstallations.save()
-  instance.save();
+  Jenkins.instance.save()
+}
+
+if (!isMavenAlreadyInstalled(mavenName)) {
+  def installation = createMavenInstallation(mavenName, mavenVersion)
+  addMavenToInstallations(installation)
 }
