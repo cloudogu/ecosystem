667dcc12c68988a5ab253db505f6ff69c39e544c
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-07-10 15:48:50 +0200

    add custom init script volume for scm #307

--- containers/scm/resources/startup.sh
+++ containers/scm/resources/startup.sh
@@ -3,6 +3,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+CUSTOM_INIT_FOLDER="/var/lib/custom.init.script.d"
+
+if [ "$(ls -A ${CUSTOM_INIT_FOLDER})" ]; then
+  cp "${CUSTOM_INIT_FOLDER}"/* /opt/scm-server/init.script.d 
+fi
+
 # create truststore, which is used in the default file
 create_truststore.sh /opt/scm-server/conf/truststore.jks > /dev/null
 
