6de38c56fd76fc9e882324f7084c0068ccc45671
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2017-07-18 10:48:06 +0200

    create always a new init script folder, to handle removed or renamed custom init scripts

--- containers/scm/resources/startup.sh
+++ containers/scm/resources/startup.sh
@@ -3,10 +3,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CUSTOM_INIT_FOLDER="/var/lib/custom.init.script.d"
+INIT_SCRIPT_FOLDER="/opt/scm-server/init.script.d"
+MAIN_INIT_SCRIPTS_FOLDER="/var/tmp/scm/init.script.d"
+CUSTOM_INIT_SCRIPTS_FOLDER="/var/lib/custom.init.script.d"
 
-if [ "$(ls -A ${CUSTOM_INIT_FOLDER})" ]; then
-  cp "${CUSTOM_INIT_FOLDER}"/* /opt/scm-server/init.script.d 
+# remove old folder to be sure, 
+# that it contains no script which is already removed from custom init script folder
+if [ -d "${INIT_SCRIPT_FOLDER}" ]; then
+  rm -rf "${INIT_SCRIPT_FOLDER}"
+fi
+
+# copy fresh main init scripts
+cp -rf "${MAIN_INIT_SCRIPTS_FOLDER}" "${INIT_SCRIPT_FOLDER}"
+
+# merge custom init scripts, if the volume is not empty
+if [ "$(ls -A ${CUSTOM_INIT_SCRIPTS_FOLDER})" ]; then
+  cp "${CUSTOM_INIT_SCRIPTS_FOLDER}"/* "${INIT_SCRIPT_FOLDER}"
 fi
 
 # create truststore, which is used in the default file
