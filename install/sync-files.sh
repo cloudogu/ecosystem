1a5e270b265e4d806b070db5d7f6d4e28526d558
Robert Auer <robert.auer@cloudogu.com>
2017-09-28 09:17:48 +0200

    Change owner of docker logrotate file to make it executable; #311

--- install/sync-files.sh
+++ install/sync-files.sh
@@ -9,6 +9,7 @@ rsync -rav $INSTALL_HOME/resources/* /
 # space for privilege adjustment
 chmod u+x /etc/ces/*.sh
 chmod o+x /usr/local/bin/ipchange.sh
+chown root:root /etc/logrotate.d/docker
 
 # Reload systemd daemon to recognize dockeroptions config file
 systemctl daemon-reload
