986940b06f54aabb6e82fe8cea94041dc0bb29a1
Robert Auer <robert.auer@cloudogu.com>
2017-10-16 09:35:20 +0200

    Moved copytrunk command to correct file; #311

--- install/sync-files.sh
+++ install/sync-files.sh
@@ -3,14 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DOCKER_LOGROTATE="/var/log/docker/*.log {
-    copytruncate
-    size 100M
-    rotate 2
-    missingok
-    compress
-}"
-
 # sync resources to fs
 rsync -rav $INSTALL_HOME/resources/* /
 
@@ -18,9 +10,6 @@ rsync -rav $INSTALL_HOME/resources/* /
 chmod u+x /etc/ces/*.sh
 chmod o+x /usr/local/bin/ipchange.sh
 chown root:root /etc/logrotate.d/docker
-
-# define logrotate with "copytruncate" to prevent deleting log-files at logrotate
-echo "$DOCKER_LOGROTATE" > /etc/logrotate.d/docker
 
 # Reload systemd daemon to recognize dockeroptions config file
 systemctl daemon-reload
