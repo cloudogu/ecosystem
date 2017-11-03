e0743a1c882d1caf9307a825e40c10e88dc9c405
Maren Süwer <maren.suewer@triology.de>
2017-11-03 08:48:02 +0100

    remove scm directory from ecosystem repository

--- containers/scm/it/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  baseUrl: 'https://demo.cloudogu.com',
-  username: 'ces-admin',
-  password: 'ecosystem2016',
-  displayName: 'admin',
-  email: 'ces-admin@cloudogu.com',
-  debug: true
-};
