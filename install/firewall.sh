9d1e38ef50b0d7e5c749822123a7102e9ab21f5f
Maren Süwer <maren.suewer@triology.de>
2017-11-07 08:54:19 +0100

    add strict mode and interpreter to firewall.sh

--- install/firewall.sh
+++ install/firewall.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 docker network create docker_gwbridge -d bridge --gateway 172.18.0.1 --subnet 172.18.0.0/16 --opt com.docker.network.bridge.enable_icc=false --opt com.docker.network.bridge.enable_ip_masquerade=true --opt com.docker.network.bridge.name=docker_gwbridge
 
 ufw allow ssh
