2b5e05e016d8114572387af563656b4347b8664b
Maren Süwer <maren.suewer@triology.de>
2017-11-07 10:10:24 +0100

    #3096: put creating docker_gwbridge to create-network.sh

--- install/firewall.sh
+++ install/firewall.sh
@@ -3,8 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# create docker_gwbridge network since it is not created automatically by the ecosystem at this step but it is needed for firewall settings
-docker network create docker_gwbridge -d bridge --gateway 172.18.0.1 --subnet 172.18.0.0/16 --opt com.docker.network.bridge.enable_icc=false --opt com.docker.network.bridge.enable_ip_masquerade=true --opt com.docker.network.bridge.name=docker_gwbridge
+
 
 # allow only ssh, http, https, 8080 and the communication through docker_gwbridge to any port 4001
 ufw allow ssh
