ec119308d01ab5364b1b4364697668005c708b45
Oliver Milke <oliver.milke@cloudogu.com>
2017-11-08 11:31:39 +0100

    #3096: fix shellcheck hint

--- install/firewall.sh
+++ install/firewall.sh
@@ -11,5 +11,5 @@ ufw allow http
 ufw allow https
 ufw allow 8080
 # set docker_gwbridge address dynamically so that this rule is always able to work after possible change of docker_gwbridge address
-ufw allow from `docker network inspect docker_gwbridge -f '{{(index .IPAM.Config 0).Subnet}}'` to any port 4001
+ufw allow from $(docker network inspect docker_gwbridge -f '{{(index .IPAM.Config 0).Subnet}}') to any port 4001
 ufw --force enable
