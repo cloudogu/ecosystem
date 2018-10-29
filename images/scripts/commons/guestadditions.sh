e3dfa71849b4c3d2ce2565251a95441733e3f926
Robert Auer <robert.auer@cloudogu.com>
2018-10-29 16:20:13 +0100

    Install virtualbox guest additions only if virtualbox is the packer builder type

--- images/scripts/commons/guestadditions.sh
+++ images/scripts/commons/guestadditions.sh
@@ -6,16 +6,8 @@ set -o pipefail
 case "$PACKER_BUILDER_TYPE" in
 
 virtualbox-iso)
-  mkdir -p /mnt/virtualbox
-  mount -o loop  ${HOME_DIR}/VBoxGuest*.iso /mnt/virtualbox
-  # encapsulate execution because of false positve (https://github.com/dotless-de/vagrant-vbguest/issues/168)
-  if sh /mnt/virtualbox/VBoxLinuxAdditions.run 
-  then
-  echo "VBoxLinuxAdditions.run was successful"
-  fi
-  ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-  umount /mnt/virtualbox
-  rm -rf ${HOME_DIR}/VBoxGuest*.iso
+  DEBIAN_FRONTEND=noninteractive apt-get -y install \
+  virtualbox-guest-dkms
   ;;
 
 vmware-iso)
