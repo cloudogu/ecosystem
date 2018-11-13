bbeb20deb6897956145ec49249ea4cd1f97be61b
Robert Auer <robert.auer@cloudogu.com>
2018-11-13 08:10:03 +0100

    Add guest additions manually

--- images/scripts/commons/guestadditions.sh
+++ images/scripts/commons/guestadditions.sh
@@ -7,7 +7,18 @@ case "$PACKER_BUILDER_TYPE" in
 
 virtualbox-iso)
   DEBIAN_FRONTEND=noninteractive apt-get -y install \
-  virtualbox-guest-dkms
+  gcc make perl
+  mkdir -p /mnt/virtualbox
+  mount -o loop  ${HOME_DIR}/VBoxGuest*.iso /mnt/virtualbox
+  # encapsulate execution because of false positve (https://github.com/dotless-de/vagrant-vbguest/issues/168)
+  if sh /mnt/virtualbox/VBoxLinuxAdditions.run
+  then
+  echo "VBoxLinuxAdditions.run was successful"
+  fi
+  # link to guest additions is already present
+  #ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+  umount /mnt/virtualbox
+  rm -rf ${HOME_DIR}/VBoxGuest*.iso
   ;;
 
 vmware-iso)
