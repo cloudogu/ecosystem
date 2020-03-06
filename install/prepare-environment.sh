6990224ccbb7ff37b8c86667c5e6417882fa9fa1
Robert Auer <robert.auer@cloudogu.com>
2020-03-06 07:12:18 +0100

    Do not add /opt/ces/bin to secure_path; #385

--- install/prepare-environment.sh
+++ install/prepare-environment.sh
@@ -3,9 +3,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# shellcheck disable=SC1091
 source /etc/ces/functions.sh
 
 # write current ip
+# shellcheck disable=SC1091
 source /etc/environment
 export PATH
 
@@ -19,29 +21,6 @@ if [ ! -d /var/log/docker ]; then
 fi
 chown syslog /var/log/docker
 systemctl restart rsyslog.service
-
-# modify sudoers save path
-echo "modify sudoers"
-if [ -f "/etc/sudoers" ]; then
-  if ! grep "/opt/ces/bin" /etc/sudoers &>/dev/null; then
-    if sed 's@secure_path="/usr@secure_path="/opt/ces/bin:@g' /etc/sudoers > /tmp/sudoers.new; then
-      if visudo -c -f /tmp/sudoers.new &>/dev/null; then
-        cp /tmp/sudoers.new /etc/sudoers
-        chown root:root /etc/sudoers
-        chmod 0440 /etc/sudoers
-      else
-        >&2 echo 'ERR: sudoers file is not valid'
-      fi
-    else
-      >&2 echo "ERR: failed to modify sudoers"
-    fi
-    rm -f /tmp/sudoers.new
-  else
-    >&2 echo 'sudoers file is already prepared'
-  fi
-else
-  >&2 echo 'ERR: sudoers file does not exists'
-fi
 
 # Enable IP change check service
 systemctl daemon-reload
