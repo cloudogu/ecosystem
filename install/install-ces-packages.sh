bf2d270233634dd1d809ffea1a62bc30e84afef6
Robert Auer <robert.auer@cloudogu.com>
2020-07-02 15:03:25 +0200

    Fix ces packages script and add more info output

--- install/install-ces-packages.sh
+++ install/install-ces-packages.sh
@@ -9,16 +9,20 @@ APTINSTALL=""
 for PKG in $PACKAGES; do
   if ls /vagrant/"${PKG}"*.deb 2> /dev/null; then
     DEBPKG=$(ls /vagrant/"${PKG}"*.deb 2> /dev/null)
-    echo "install ${PKG} from development package ${DEBPKG}"
+    echo "installing ${PKG} from development package ${DEBPKG}..."
     dpkg -i "${DEBPKG}"
   else
-    echo "install ${PKG} from apt repository"
+    echo "Added ${PKG} to list to install from apt repository"
     APTINSTALL="${APTINSTALL} ${PKG}"
   fi
 done
 
 if [ "${APTINSTALL}" != "" ]; then
+  echo "Updating ces package list..."
   apt-get update -o Dir::Etc::sourcelist="sources.list.d/ces.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-  apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages "${APTINSTALL}"
+  echo "Installing ces packages ${APTINSTALL}..."
+  # Word splitting is okay at this point as there may be multiple package names in ${APTINSTALL}
+  # shellcheck disable=SC2086
+  apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages ${APTINSTALL}
 fi
 
