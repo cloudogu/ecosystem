4958c8a8a20a77a8bbb40c8e2d6792853e2e1c56
Simon Klein <simon.klein@cloudogu.com>
2020-10-27 15:57:06 +0100

    #413 Update dockeroptions and docker start script

--- resources/etc/systemd/system/docker.service.d/docker-start-script.sh
+++ resources/etc/systemd/system/docker.service.d/docker-start-script.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
+function get_config_for(){
+  PROTOCOL=$1
+  USERNAME=$2
+  PASSWORD=$3
+  HOST=$4
+  PORT=$5
 
-HTTPS_PROXY_USERNAME="$(etcdctl get config/nexus/proxyConfiguration/https/authentication/username)"
-HTTPS_PROXY_PASSWORD="$(etcdctl get config/nexus/proxyConfiguration/https/authentication/password)"
-HTTPS_PROXY_HOST="$(etcdctl get config/nexus/proxyConfiguration/https/host)"
-HTTPS_PROXY_PORT="$(etcdctl get config/nexus/proxyConfiguration/https/port)"
-HTTPS_PROXY_CONFIG="https://${HTTPS_PROXY_USERNAME}:${HTTPS_PROXY_PASSWORD}@${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT}"
+  echo ""
+}
 
-HTTP_PROXY_USERNAME="$(etcdctl get config/nexus/proxyConfiguration/http/authentication/username)"
-HTTP_PROXY_PASSWORD="$(etcdctl get config/nexus/proxyConfiguration/http/authentication/password)"
-HTTP_PROXY_HOST="$(etcdctl get config/nexus/proxyConfiguration/http/host)"
-HTTP_PROXY_PORT="$(etcdctl get config/nexus/proxyConfiguration/http/port)"
-HTTP_PROXY_CONFIG="http://${HTTP_PROXY_USERNAME}:${HTTP_PROXY_PASSWORD}@${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT}"
+echo "collecting https steps"
+HTTPS_PROXY_USERNAME="$(etcdctl get config/nexus/proxyConfiguration/https/authentication/username)" || true
+HTTPS_PROXY_PASSWORD="$(etcdctl get config/nexus/proxyConfiguration/https/authentication/password)" || true
+HTTPS_PROXY_HOST="$(etcdctl get config/nexus/proxyConfiguration/https/host)" || true
+HTTPS_PROXY_PORT="$(etcdctl get config/nexus/proxyConfiguration/https/port)" || true
+#HTTPS_PROXY_CONFIG="https://${HTTPS_PROXY_USERNAME}:${HTTPS_PROXY_PASSWORD}@${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT}" || true
+HTTPS_PROXY_CONFIG="$(get_config_for "https" "${HTTPS_PROXY_USERNAME}" "${HTTPS_PROXY_PASSWORD}" "${HTTPS_PROXY_HOST}" "${HTTPS_PROXY_PORT}")" || true
 
-PROXY_EXCLUSIONS="$(etcdctl get config/nexus/proxyConfiguration/nonProxyHosts)"
 
-PROXY_SETTINGS="--env HTTP_PROXY=\"${HTTP_PROXY_CONFIG}\" --env HTTPS_PROXY=\"${HTTPS_PROXY_CONFIG}\" --env NO_PROXY=\"${PROXY_EXCLUSIONS}\""
+echo "collecting http steps"
+HTTP_PROXY_USERNAME="$(etcdctl get config/nexus/proxyConfiguration/http/authentication/username)" || true
+HTTP_PROXY_PASSWORD="$(etcdctl get config/nexus/proxyConfiguration/http/authentication/password)" || true
+HTTP_PROXY_HOST="$(etcdctl get config/nexus/proxyConfiguration/http/host)" || true
+HTTP_PROXY_PORT="$(etcdctl get config/nexus/proxyConfiguration/http/port)" || true
+#HTTP_PROXY_CONFIG="http://${HTTP_PROXY_USERNAME}:${HTTP_PROXY_PASSWORD}@${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT}"
+HTTP_PROXY_CONFIG=$(get_config_for "http" "${HTTP_PROXY_USERNAME}" "${HTTP_PROXY_PASSWORD}" "${HTTP_PROXY_HOST}" "${HTTP_PROXY_PORT}") || true
 
-echo "${PROXY_SETTINGS}" > /myfile
+#PROXY_EXCLUSIONS="$(etcdctl get config/nexus/proxyConfiguration/nonProxyHosts)"
 
+echo "generating proxy settings"
+PROXY_SETTINGS="${HTTP_PROXY_CONFIG} ${HTTPS_PROXY_CONFIG}"
+PROXY_SETTINGS="--env HTTP_PROXY=\"\""
+
+echo "actually starting dockerd"
+echo "settings: '${PROXY_SETTINGS}'"
 /usr/bin/dockerd -s btrfs --log-driver=syslog --cluster-store=etcd://127.0.0.1:4001 --cluster-advertise=127.0.0.1:4001 "${PROXY_SETTINGS}"
