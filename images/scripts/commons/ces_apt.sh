df464eac8b0e7f361b1cbed1c5c997cf1431a736
Robert Auer <robert.auer@cloudogu.com>
2021-03-19 12:23:07 +0100

    Move ces apt repo configuration to its own script; #427

---
+++ images/scripts/commons/ces_apt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Adding Cloudogu apt repository..."
+
+if [ ! -d /etc/apt/sources.list.d ]; then
+  mkdir -p /etc/apt/sources.list.d
+fi
+
+echo "deb [arch=amd64] https://apt.cloudogu.com/ces/ focal main" > /etc/apt/sources.list.d/ces.list
+
+# import cloudogu key
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0249BCED
+
+# update package index only for ces repository
+apt-get update -o Dir::Etc::sourcelist="sources.list.d/ces.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
--- images/scripts/commons/etcd.sh
+++ images/scripts/commons/etcd.sh
@@ -3,19 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# This script relies on Cloudogu apt repository beeing present. See ces_apt.sh
+
 echo "installing etcd - start"
-
-if [ ! -d /etc/apt/sources.list.d ]; then
-  mkdir -p /etc/apt/sources.list.d
-fi
-
-echo "deb [arch=amd64] https://apt.cloudogu.com/ces/ bionic main" > /etc/apt/sources.list.d/ces.list
-
-# import cloudogu key
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0249BCED
-
-# update package index only for ces repository
-apt-get update -o Dir::Etc::sourcelist="sources.list.d/ces.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 
 # install etcd
 apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages ces-etcd
