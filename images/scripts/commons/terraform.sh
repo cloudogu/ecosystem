8f052eaac90402b2ac8ae74eb54f19451aa1ecef
Robert Auer <robert.auer@cloudogu.com>
2023-02-01 10:22:53 +0100

    Upgrade hashicorp apt repo installation

--- images/scripts/commons/terraform.sh
+++ images/scripts/commons/terraform.sh
@@ -5,22 +5,21 @@ set -o pipefail
 
 # Install terraform
 # See https://learn.hashicorp.com/tutorials/terraform/install-cli
+# See https://www.hashicorp.com/official-packaging-guide
 
 TERRAFORM_VERSION=0.15.4
 
 export DEBIAN_FRONTEND=noninteractive
 
 echo "installing terraform"
-# Add Hashicorps’s official GPG key
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-# Verify key
-KEY=$(sudo apt-key fingerprint A3219F7B)
-if [ -z "${KEY}" ]; then
-  echo "Hashicorp key has not been added successfully";
-  exit 1
+# Download the signing key to a new keyring
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# Verify the key's fingerprint
+if ! (gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint | grep "798A EC65 4E5C 1542 8C8E 42EE AA16 FCBC A621 E701"); then
+  echo "Could not verify hashicorp key fingerprint"
 fi
 # Add repository
-echo "deb [arch=amd64] https://apt.releases.hashicorp.com focal main" > /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 # Install Terraform
 apt-get -y update
 apt-get -y install terraform=${TERRAFORM_VERSION}
