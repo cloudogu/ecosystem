995c297d226a7554976a42bb618704940e6f26a6
Robert Auer <robert.auer@cloudogu.com>
2025-03-05 13:57:46 +0100

    Do not remove absent packages

--- images/scripts/commons/cleanup.sh
+++ images/scripts/commons/cleanup.sh
@@ -12,12 +12,6 @@ dpkg --list \
 # Delete X11 libraries
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
 
-# Delete obsolete networking
-apt-get -y purge ppp pppconfig pppoeconf;
-
-# Delete oddities
-apt-get -y purge popularity-contest;
-
 apt-get -y autoremove;
 apt-get -y clean;
 
